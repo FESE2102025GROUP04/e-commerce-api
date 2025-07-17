689a71471b9b01818941a26a215318a93e0d457f
CheaLivchea <6023010052@camtech.edu.kh>
2025-07-17 14:27:43 +0700

    back to normal

--- controllers/productController/addProduct.js
+++ controllers/productController/addProduct.js
@@ -1,34 +1,16 @@
-//Testing branch - Modified with conflicts
-
 const db = require('../../models')
 
 const addProduct = async (req, res) => {
     try {
-      // Conflicting variable names
-      const product = req.body;
-      const products = req.body.items; // This might be undefined
-      
+      const product = req.body;  
       console.log("Received products: ", product);
-      console.log("Also received: ", products);
 
-      // Trying to use both variables inconsistently
-      if (product && products) {
-        const productTb = await db.Product.bulkCreate(products); // Using products instead of product
-        return res.json(productTb);
-      } else {
-        const singleProduct = await db.Product.create(product); // Different method for single item
-        return res.json(singleProduct);
-      }
+      const productTb = await db.Product.bulkCreate(product);
       
-      // Unreachable code
-      console.log("This will never execute");
-      
+      return res.json(productTb);
     } catch (err) {
-      console.log("Error occurred but not handling properly");
       return res.status(500).json({ error: err.message });  
     }
-};
-
-// Conflicting export
-module.exports = { addProduct };
-module.exports.addProduct = addProduct; // Duplicate export
+  };
+  
+  module.exports = { addProduct };
